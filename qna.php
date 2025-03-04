ba0d2311d34e608c6bd7f90a65a0cf903c6633aa
Livia <livia.kelebercova@gmail.com>
2025-03-04 13:43:22 +0100

    header & footer templates

---
+++ qna.php
@@ -0,0 +1,36 @@
+<?php
+    include('partials/header.php');
+?>
+<main>
+  <section class="banner">
+    <div class="container text-white">
+      <h1>Q&A</h1>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-100 text-center">
+        <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em></strong></p>
+      </div>
+    </div>
+  </section>
+    <section class="container">
+    <div class="accordion">
+      <div class="question">Otázka 1</div>
+      <div class="answer">Odpoveď 1</div>
+    </div>
+    <div class="accordion">
+      <div class="question">Otázka 2</div>
+      <div class="answer">Odpoveď 2</div>
+    </div>
+    <div class="accordion">
+      <div class="question">Otázka 3</div>
+      <div class="answer">Odpoveď 3</div>
+    </div>
+  </section>
+  </section>
+</div>
+</main>
+<?php
+    include('partials/footer.php');
+?>
---
+++ thankyou.php
@@ -0,0 +1,20 @@
+<?php
+    include('partials/header.php');
+?>
+<main>
+  <section class="banner">
+    <div class="container text-white">
+      <h1>Ďakujeme</h1>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-100 text-center">
+          <h2>Ďakujeme za vyplnenie formulára</h2>
+      </div>
+    </div>
+  </section>
+</main>
+<?php
+    include('partials/footer.php');
+?>
